3756b44fa44d98231a4fc8af261ffbaa3d71d4c3
Delowar-Khan <50891528+Delowar-Khan@users.noreply.github.com>
2022-05-21 13:25:25 +0600

    Add files via upload

---
+++ Lab4A/action_page.php
@@ -0,0 +1,54 @@
+<?php
+    include('database_connect.php');
+
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $dept = $_POST['dept'];
+    $roll = $_POST['roll'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $phone = $_POST['tel'];
+    $address = $_POST['address'];
+    $district = $_POST['district'];
+    $flavour_list = $_POST['flavour'];
+
+    $ice_cream = "";
+    foreach ($flavour_list as $x){ 
+        $ice_cream = $ice_cream.' ' .$x;
+    }
+
+    
+
+    $sql = "INSERT INTO person (fname, lname, dept, roll, gender, email, phone, flavour, address, district)
+        VALUES ('$fname', '$lname', '$dept', '$roll', '$gender', '$email', '$phone', '$ice_cream','$address', '$district')";
+    
+
+    if ($conn->query($sql) === TRUE) {
+        header('location: index.php');
+        echo "New record created successfully<br>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $sql = "SELECT * FROM Person";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br>First Name: " . $row["fname"]. "<br>Last Name: " . $row["lname"];
+        echo "<br>Department: " . $row["dept"]."<br>Roll: " . $row["roll"];
+        echo "<br>Email: " . $row["email"] . "<br>Gender: " . $row["gender"];
+        echo "<br>Phone: " . $row["phone"] . "<br>Flavour: " . $row["flavour"];
+        echo "<br>Address: " . $row["address"]. "<br>District: " . $row["district"]. "<br>";
+        
+    }
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
+
+
+?>
+    
