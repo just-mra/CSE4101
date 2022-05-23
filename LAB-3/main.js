95884a6d07cb223e778ba5ddf4d094a54b020036
Sumaiya Sadia Bristy <52797747+SumaiyaSadiaBristy@users.noreply.github.com>
2022-05-23 12:02:20 +0600

    Add files via upload

---
+++ LAB-3/main.js
@@ -0,0 +1,43 @@
+function nameFunction() {
+    var x = document.getElementById("nameinp");
+
+    // x.value = x.value.toUpperCase();
+    var name = [];
+    var a=0;
+    name[0]=x.value[0].toUpperCase();
+ for (let i = 1; i < x.value.length; i++) {
+     
+     const element = x.value[i];
+
+     if (element == " ") {
+         a=1;
+        //   x.value[i+1]=  x.value[i+1];
+        name[i] = x.value[i]
+
+        name[i+1]= x.value[i+1].toUpperCase()
+        i++;
+     }
+     else{
+        name[i] = x.value[i]
+
+     }
+ }
+ if (a==0) {
+    alert('There Should Be A First Name And Last Name');
+ }
+let ans = name.join("");
+x.value= ans;
+
+}
+
+var nums = ["Vanilla", "Chocolate", "Green Tea", "Strawberry", "Mango" , "Avocado"],    
+    ranNums = [],
+    i = nums.length,
+    j = 0;
+
+while (i--) {
+    j = Math.floor(Math.random() * (i+1));
+    ranNums.push(nums[j]);
+    nums.splice(j,1);
+}
+
