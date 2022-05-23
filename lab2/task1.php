b6185ed0454ce08971cc483abbf7be79f32d3d5b
farihatasnim <tfariha92@gmail.com>
2022-05-23 07:48:56 -0700

    Add files via upload

---
+++ lab2/task1.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>My first Website</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div id="wrapper">
+  <header>
+      
+    <div id="header">
+       <div class="headerimg">
+        <img src="img/logo.png" alt="logo">
+       </div>
+       <div class="title">
+         <h2>Web Site Title</h2>
+       </div>
+     </div>
+
+  </header>
+
+
+  <nav>
+    <div id="body">
+         <div class="nav">
+          <ul>
+          <li><a class="active" href="#">   Menu</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Cv</a></li>
+          <li><a href="#">Send Message</a></li>
+          
+          </ul>
+        </div>
+        
+        <div class="menudetails">
+
+         <div class="profile">
+                      <div class="profileimg">
+                        <img src="img/team3.jpg" alt="man"/>
+                        <p>John Doe</p>
+                        <p>johndice14@gmail.com</p>
+                        <p>Unites State</p>
+                      </div>
+
+                   <div class="profileactivity">
+                    <div class="inbox">
+   <p><a href="#"><img alt="Qries" src="img/inbox.png" width="50" height="50"><p>Inbox</p></a><p>
+                    </div>
+                    <div class="seeting">
+   <a href="#"><img alt="Qries" src="img/setting.png" width="50" height="50"><p>Setting</p></a>
+                    </div>
+                    <div class="profile1">
+   <a href="#"><img alt="Qries" src="img/profile.png" width="50" height="50"><p>Profile</p></a>
+                        </div>
+                   </div>
+        </div>
+         <div class="profilemenu">
+                      <div class="coursenav">
+                        <ul>
+             <li><a class="active" href="#home">My Courses</a></li>
+             <li><a href="#news">My Questions</a></li>
+             <li><a href="#contact">My Answers</a></li>
+             
+                      </ul>
+                      </div>
+            <div class="course">
+                   <p><img alt="Qries" src="img/book.png" width="15" height="15">&emsp;ANAT 101-Introductory Human Anatomy&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Not Yetrated</p>
+                   <p><img alt="Qries" src="img/book.png" width="15" height="15">&emsp;ANAT 101-Introductory Human Anatomy&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Not Yetrated</p>
+                   <p><img alt="Qries" src="img/book.png" width="15" height="15">&emsp;ANAT 101-Introductory Human Anatomy&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Not Yetrated</p>
+                   <p><img alt="Qries" src="img/book.png" width="15" height="15">&emsp;ANAT 101-Introductory Human Anatomy&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Not Yetrated</p>
+                   <p><img alt="Qries" src="img/book.png" width="15" height="15">&emsp;ANAT 101-Introductory Human Anatomy&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Not Yetrated</p>
+            </div>         
+
+                   </div>
+    </div>
+  </nav>
+  
+   
+
+ </div>
+</body>
+</html>
---
+++ lab2/task2.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>My first Website</title>
+  <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+  <div id="wrapper">
+  <header>
+      
+    <div id="header">
+       <div class="headerimg">
+        <img src="img/logo.png" alt="logo">
+       </div>
+       <div class="title">
+         <h2>Web Site Title</h2>
+       </div>
+     </div>
+
+  </header>
+
+
+  <nav>
+    <div id="body">
+         <div class="nav">
+          <ul>
+          <li><a class="active" href="#">   Menu</a></li>
+           <li><a href="#">Home</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Cv</a></li>
+          <li><a href="#">Send Message</a></li>
+          
+          </ul>
+        </div>
+        
+        <div class="menudetails">
+          <p>chat</p>
+
+          <img src="img/chat.jpg" alt="chatimage"/>
+    </div>
+  </nav>
+  
+   
+
+ </div>
+</body>
+</html>
